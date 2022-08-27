5a923823ecd39188125f82997dd6592566bc1915
Mohammad <iimoha46@gmail.com>
2022-08-27 12:48:13 +0300

    prio test

--- comps/PersonCard.jsx
+++ comps/PersonCard.jsx
@@ -34,7 +34,7 @@ const PersonCard = (props) => {
 
             <div className={styles.wrapper}>
                 <div className={styles.pic}>
-                    <Image className={styles.avatar} src={props.pic} alt="person" layout='fill' objectFit='cover'/>
+                    <Image className={styles.avatar} src={props.pic} alt="person" layout='fill' objectFit='cover' priority/>
                 </div>
             </div>
 
